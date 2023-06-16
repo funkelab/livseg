28f480516cb74847db26496e60aab02b2d56809e
Diane Adjavon <adjavond@hhmi.org>
2023-06-16 11:55:05 -0400

    Add dask for writing files

--- data/convert_to_zarr.py
+++ data/convert_to_zarr.py
@@ -1,11 +1,14 @@
 from PIL import Image
 from PIL.TiffTags import TAGS_V2
+from imageio import imread
 import sys
 from pathlib import Path
 from natsort import natsorted
 import zarr
 import numpy as np
 from tqdm import tqdm
+from time import perf_counter
+import dask.array as da
 
 
 Image.MAX_IMAGE_PIXELS = None
@@ -35,54 +38,47 @@ if __name__ == "__main__":
 
     path = Path(sys.argv[3])
 
-    filenames = [file.name for file in path.iterdir() if file.is_file()]
+    filenames = [filename for filename in path.iterdir() if filename.is_file()]
 
-    for name in natsorted(filenames):
-        if name[-6:-4] == "00":
-            c0.append(name)
-        elif name[-6:-4] == "01":
-            c1.append(name)
-        elif name[-6:-4] == "02":
-            c2.append(name)
+    for filename in natsorted(filenames):
+        channel = filename.name[-6:-4]
+        if channel == "00":
+            c0.append(filename)
+        elif channel == "01":
+            c1.append(filename)
+        elif channel == "02":
+            c2.append(filename)
         else:
-            c3.append(name)
+            c3.append(filename)
 
     print("creating zarr datatset")
     zfile = zarr.open(target_directory/"livseg_data.zarr", mode="a")
 
     # Get the first file
-    print("obtaining meta data")
-    filenames = list(source_directory.iterdir())
-    image, meta = readImage(filenames[0])
-
-    shape = (len(filenames), meta["ImageWidth"], meta["ImageLength"])
-    dtype = np.asarray(image).dtype
-
-    dapi_data = zfile.require_dataset(
-        "dapi", shape=shape, chunks=(1, 128, 128), dtype=dtype)
-    channel_zero = zfile.require_dataset(
-        "c0", shape=shape, chunks=(1, 128, 128), dtype=dtype)
-    channel_one = zfile.require_dataset(
-        "c1", shape=shape, chunks=(1, 128, 128), dtype=dtype)
-    channel_two = zfile.require_dataset(
-        "c2", shape=shape, chunks=(1, 128, 128), dtype=dtype)
-    channel_three = zfile.require_dataset(
-        "c3", shape=shape, chunks=(1, 128, 128), dtype=dtype)
-
-    for key, value in meta.items():
-        dapi_data.attrs[key] = value
-        channel_zero.attrs[key] = value
-        channel_one.attrs[key] = value
-        channel_two.attrs[key] = value
-        channel_three.attrs[key] = value
-
     print("reading/writing files")
 
-    for channel, dataset in zip([filenames, c0, c1, c2, c3], [dapi_data, channel_zero, channel_one, channel_two, channel_three]):
+    for channel, dsname in zip(
+            [filenames, c0, c1, c2, c3],
+            ["dapi", "c0", "c1", "c2", "c3"]
+    ):
 
-        for z, name in tqdm(enumerate(natsorted(channel)), total=len(channel)):
+        print("obtaining meta data")
+        image, meta = readImage(channel[0])
+
+        images = []
+        for z, name in tqdm(enumerate(natsorted(channel)),
+                            total=len(channel), desc=dsname):
             # Read the data in the file
-            image, _ = readImage(name)
-
-            # Save the data in the zarr dataset
-            dataset[z] = np.asarray(image).T
+            image = imread(name)
+            images.append(image.T)
+        full_stack = np.stack(images)
+        # Save the data in the zarr dataset
+        array = da.from_array(full_stack, chunks=(32, 32, 32))
+        t0 = perf_counter()
+        array.to_zarr(target_directory / "livseg_data.zarr", dsname,
+                      overwrite=True)
+        print(f"Dataset {dsname} took {perf_counter() - t0}s to save")
+        # Add metadata
+        for key, value in meta.items():
+            # TODO check with Caleb which attributes he needs
+            zfile[dsname].attrs[key] = value
