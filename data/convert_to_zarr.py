fe5cf6335dbd65307624579432a33928df141c28
ananyasista21 <ananya.sista@gmail.com>
2023-06-22 14:59:13 -0400

    speed up the data conversion

--- data/convert_to_zarr.py
+++ data/convert_to_zarr.py
@@ -8,20 +8,21 @@ import zarr
 import numpy as np
 from tqdm import tqdm
 from time import perf_counter
-import dask.array as da
 
 
 Image.MAX_IMAGE_PIXELS = None
 
 
-def readImage(filepath):
+def read_image_meta_data(filepath):
     image = Image.open(filepath)
     meta_data = {TAGS_V2[key].name: image.tag_v2[key]
                  for key in image.tag_v2}
-    return image, meta_data
+    return meta_data
 
 
 if __name__ == "__main__":
+
+    print("Starting :)")
 
     assert len(sys.argv) == 4, "Needs to have 3 Arguments"
 
@@ -31,54 +32,60 @@ if __name__ == "__main__":
     # Create a zarr file
     target_directory = Path(sys.argv[2])
 
-    c0 = []
-    c1 = []
-    c2 = []
-    c3 = []
+    c0_filenames = []
+    c1_filenames = []
+    c2_filenames = []
+    c3_filenames = []
 
     path = Path(sys.argv[3])
 
-    filenames = [filename for filename in path.iterdir() if filename.is_file()]
+    print("Searching for channel files...")
 
-    for filename in natsorted(filenames):
-        channel = filename.name[-6:-4]
-        if channel == "00":
-            c0.append(filename)
-        elif channel == "01":
-            c1.append(filename)
-        elif channel == "02":
-            c2.append(filename)
+   #  dapi_filenames = [filename for filename in source_directory.iterdir() if filename.is_file()]
+    other_channel_filenames = [filename for filename in path.iterdir() if filename.is_file()]
+
+    print("Separating other channels...")
+
+    for filename in tqdm(natsorted(other_channel_filenames)):
+        ch_id = filename.name[-6:-4]
+        if ch_id == "00":
+            c0_filenames.append(filename)
+        elif ch_id == "01":
+            c1_filenames.append(filename)
+        elif ch_id == "02":
+            c2_filenames.append(filename)
         else:
-            c3.append(filename)
+            c3_filenames.append(filename)
 
     print("creating zarr datatset")
-    zfile = zarr.open(target_directory/"livseg_data.zarr", mode="a")
+    zarr_container = zarr.open(target_directory / "livseg_data.zarr", "a")
 
     # Get the first file
     print("reading/writing files")
 
-    for channel, dsname in zip(
-            [filenames, c0, c1, c2, c3],
-            ["dapi", "c0", "c1", "c2", "c3"]
+    for channel_filenames, dsname in zip(
+            [c3_filenames],
+            ["c3"]
     ):
 
         print("obtaining meta data")
-        image, meta = readImage(channel[0])
+        meta_data = read_image_meta_data(channel_filenames[0])
+        print(meta_data)
 
-        images = []
-        for z, name in tqdm(enumerate(natsorted(channel)),
-                            total=len(channel), desc=dsname):
-            # Read the data in the file
-            image = imread(name)
-            images.append(image.T)
-        full_stack = np.stack(images)
+        print("Reading images...")
+        filenames = natsorted(channel_filenames)
+        images = [
+            imread(filename).T
+            for filename in tqdm(filenames, desc=dsname)
+        ]
+
         # Save the data in the zarr dataset
-        array = da.from_array(full_stack, chunks=(32, 32, 32))
-        t0 = perf_counter()
-        array.to_zarr(target_directory / "livseg_data.zarr", dsname,
-                      overwrite=True)
-        print(f"Dataset {dsname} took {perf_counter() - t0}s to save")
+        print("Saving to zarr...")
+        zarr_container[dsname] = np.stack(images)
+
         # Add metadata
-        for key, value in meta.items():
-            # TODO check with Caleb which attributes he needs
-            zfile[dsname].attrs[key] = value
+        zarr_container[dsname].attrs["resolution"] = (
+            1,  # TODO: just a guess, need to replace with correct value later
+            int(meta_data["YResolution"]),
+            int(meta_data["XResolution"])
+        )
